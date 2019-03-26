9c1f5cfdb6ee265efb6ced3277dfd70cabbfab83
Arnab Chanda <arnabchanda964@gmail.com>
2019-03-26 20:45:29 +0530

    conditions on return

--- image_switcher/lib/images.dart
+++ image_switcher/lib/images.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
 
-class Images extends StatelessWidget{
-
+class Images extends StatelessWidget {
   final String name;
   Images(this.name);
 
   @override
   Widget build(BuildContext context) {
-    return name!=""?
-            Card(
-              color: Colors.purple[600],
-              
-              child: Column(
-                children: <Widget>[
-                Image.asset(name),
-                Text(name)
-              ]),
-            ):Card(child:Center(child:Column(children: <Widget>[Text("Nothing to show!")],)));
-            }
+    return name != ""
+        ? Card(
+            color: Colors.purple[600],
+            child: Column(children: <Widget>[Image.asset(name), Text(name)]),
+          )
+        : Card(
+            color: Colors.purple[600],
+            child: Center(
+                child: Column(
+              children: <Widget>[Text("Nothing to show!")],
+            )));
   }
-
-
-
+}
